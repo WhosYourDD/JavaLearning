d798826035cde5f783fb47a893191c9e96ff2a12
David <1595582092@qq.com>
2022-04-12 16:15:01 +0800

    创建mapclass.java文件

---
+++ src/main/java/algorithm/MapClass.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class MapClass {
+    HashMap hashMap=new HashMap();
+    TreeMap treeMap=new TreeMap();
+    Hashtable hashtable=new Hashtable();
+    LinkedHashMap linkedHashMap=new LinkedHashMap();
+}
